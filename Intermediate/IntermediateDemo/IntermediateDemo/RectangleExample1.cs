2952308dd891e6c89b9112321af01b8b783d25c9
shpolina <polina@careevolution.com>
2019-05-28 18:19:23 -0400

    add intermediate project

---
+++ Intermediate/IntermediateDemo/IntermediateDemo/RectangleExample1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntermediateDemo
+{
+    public class RectangleExample1 : RectangleBase
+    {
+        public RectangleExample1(int height, int widgth)
+        {
+            Height = height;
+            Widgth = widgth;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("Widgth: " + Widgth);
+        }
+    }
+}
---
+++ Intermediate/IntermediateDemo/IntermediateDemo/RectangleExample2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IntermediateDemo
+{
+    public class RectangleExample2 : IShape
+    {
+        public RectangleExample2(int height, int widgth)
+        {
+            Height = height;
+            Widgth = widgth;
+        }
+
+        public int Area()
+        {
+            return Height * Widgth;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("Widgth: " + Widgth);
+        }
+
+        public int Height { get; protected set; }
+        public int Widgth { get; protected set; }
+    }
+}
